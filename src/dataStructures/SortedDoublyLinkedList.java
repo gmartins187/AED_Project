bd5be477d4c5ba18e68da0d897515376fa78b734
dreca <andre.jordao.amante@gmail.com>
2025-10-14 19:43:01 +0100

    métodos da sorted doubly linked list

--- src/dataStructures/SortedDoublyLinkedList.java
+++ src/dataStructures/SortedDoublyLinkedList.java
@@ -12,31 +12,44 @@ import dataStructures.exceptions.*;
  */
 public class SortedDoublyLinkedList<E> implements SortedList<E> {
 
+
     /**
      *  Node at the head of the list.
      */
     private DoublyListNode<E> head;
+
+
     /**
      * Node at the tail of the list.
      */
     private DoublyListNode<E> tail;
+
+
     /**
      * Number of elements in the list.
      */
     private int currentSize;
+
+
     /**
      * Comparator of elements.
      */
     private final Comparator<E> comparator;
+
+
     /**
      * Constructor of an empty sorted double linked list.
      * head and tail are initialized as null.
      * currentSize is initialized as 0.
      */
     public SortedDoublyLinkedList(Comparator<E> comparator) {
-        //TODO: Left as an exercise.
+        //TODO
+        this.head = null;
+        this.tail = null;
+        this.currentSize = 0;
         this.comparator = comparator;
     }
+
 
     /**
      * Returns true iff the list contains no elements.
@@ -46,14 +59,15 @@ public class SortedDoublyLinkedList<E> implements SortedList<E> {
         return currentSize==0;
     }
 
+
     /**
      * Returns the number of elements in the list.
      * @return number of elements in the list
      */
-
     public int size() {
         return currentSize;
     }
+
 
     /**
      * Returns an iterator of the elements in the list (in proper sequence).
@@ -63,15 +77,20 @@ public class SortedDoublyLinkedList<E> implements SortedList<E> {
         return new DoublyIterator<>(head);
     }
 
+
     /**
      * Returns the first element of the list.
      * @return first element in the list
      * @throws NoSuchElementException - if size() == 0
      */
     public E getMin( ) {
-        //TODO: Left as an exercise.
-        return null;
+        //TODO
+        if (size() == 0 ){
+            throw new NoSuchElementException();
+        }
+        return head.getElement();
     }
+
 
     /**
      * Returns the last element of the list.
@@ -79,17 +98,34 @@ public class SortedDoublyLinkedList<E> implements SortedList<E> {
      * @throws NoSuchElementException - if size() == 0
      */
     public E getMax( ) {
-        //TODO: Left as an exercise.
-        return null;
+        //TODO
+        if (size() == 0 ){
+            throw new NoSuchElementException();
+        }
+        return tail.getElement();
     }
+
+
     /**
      * Returns the first occurrence of the element equals to the given element in the list.
      * @return element in the list or null
      */
     public E get(E element) {
-        //TODO: Left as an exercise.
+        //TODO
+        if (head == null) {
+            return null;
+        }
+        DoublyListNode<E> current = head;
+        while (current != null) {
+            if (comparator.compare(current.getElement(), element) == 0) {
+                return current.getElement(); }
+            else if (comparator.compare(current.getElement(), element) > 0) {
+                current = current.getNext(); }
+            else break;
+        }
         return null;
     }
+
 
     /**
      * Returns true iff the element exists in the list.
@@ -98,9 +134,21 @@ public class SortedDoublyLinkedList<E> implements SortedList<E> {
      * @return true iff the element exists in the list.
      */
     public boolean contains(E element) {
-        //TODO: Left as an exercise.
-        return true;
+        //TODO
+        if (head == null) {
+            return false;
+        }
+        DoublyListNode<E> current = head;
+        while (current != null) {
+            if (comparator.compare(current.getElement(), element) == 0) {
+                return true; }
+            else if (comparator.compare(current.getElement(), element) > 0) {
+                current = current.getNext(); }
+            else break;
+        }
+        return false;
     }
+
 
     /**
      * Inserts the specified element at the list, according to the natural order.
@@ -110,6 +158,7 @@ public class SortedDoublyLinkedList<E> implements SortedList<E> {
     public void add(E element) {
         //TODO: Left as an exercise.
     }
+
 
     /**
      * Removes and returns the first occurrence of the element equals to the given element in the list.
